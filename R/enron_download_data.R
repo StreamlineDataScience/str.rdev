676aab4c51d9b5b971f33762c0b309ca9dae5087
Ivan Millanes <ivanmillanes26@gmail.com>
2023-03-07 16:26:44 -0300

    feat: update documentation

--- R/enron_download_data.R
+++ R/enron_download_data.R
@@ -1,3 +1,15 @@
+#' Download enron data
+#'
+#' @param source Character. Indicates where to download enron data from.
+#' Possible options are `"github"` and `"google_sheets"`.
+#' @param link Character. Link to the `.xls` enron file.
+#' @param destfile Character. Filepath where the `.xls` file should be stored.
+#'
+#' @details This function is used to donwload enron `.xls` data.
+#' By default, i.e. when no arguments are passed to the function, it will
+#' download data from GitHub and save it in `"inst/extdata/enron_raw_data.xls"`.
+#' Default `link`s for different `source`s were defined in `R/constants.R`.
+#'
 #' @export
 enron_download_data = function(source = c("github", "google_sheets"),
                                link = NULL,
@@ -13,12 +25,24 @@ enron_download_data = function(source = c("github", "google_sheets"),
          google_sheets = download_google_sheets(link, destfile))
 }
 
+#' Get default download link
+#'
+#' @inheritParams enron_download_data
+#'
+#' @details This function is used to get download links when `link = NULL`.
+#' Default `link`s for different `source`s were defined in `R/constants.R`.
 get_default_link <- function(source) {
   switch(source,
          github = DEFAULT_GITHUB_LINK,
          google_sheets = DEFAULT_GOOGLE_SHEETS_LINK)
 }
 
+#' Download enron data from GitHub
+#'
+#' @inheritParams enron_download_data
+#'
+#' @details This is a helper function that gets called when`source = "github"`.
+#' To download the file from GitHub, the string `?raw=true` needed to be added.
 download_github <- function(link, destfile) {
   raw_link <- glue::glue("{link}?raw=true")
   req <- httr::GET(raw_link, httr::write_disk(path = destfile, overwrite = TRUE))
@@ -27,6 +51,11 @@ download_github <- function(link, destfile) {
     stop("Error: Github download failed")
 }
 
+#' Download enron data from Google Sheets
+#'
+#' @inheritParams enron_download_data
+#'
+#' @details This is a helper function that gets called when `source = "google_sheets"`.
 download_google_sheets <- function(link, destfile) {
   tryCatch({
     suppressMessages(
