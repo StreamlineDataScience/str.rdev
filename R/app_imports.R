09706bf5a208022a2ea925d4f99b210ba2ff401d
Ivan Millanes <ivanmillanes26@gmail.com>
2023-03-08 08:56:19 -0300

    feat: remove warnings from app packages imports

---
+++ R/app_imports.R
@@ -0,0 +1,7 @@
+#' @importFrom bs4Dash dashboardPage dashboardHeader dashboardBrand dashboardSidebar sidebarMenu menuItem dashboardBody tabItems tabItem box progressBar
+#' @import echarts4r
+#' @import htmlwidgets
+#' @import lubridate
+#' @importFrom shiny tagList h1 p a strong reactive shinyApp
+#' @importFrom shinyWidgets pickerInput
+NULL
