2bba449e513a12d39402a01770fbd8c17c8fd723
Ivan Millanes <ivanmillanes26@gmail.com>
2023-03-08 08:21:40 -0300

    feat: document enron data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Processed Enron data
+#'
+#' Enron was a gas pipeline company. The data contains deliveries and receipts
+#' for a number of locations/facilities (e.g.ACADIAN, BRIDGELINE).
+#'
+#' @format ## `enron_data`
+#' A tibble with 2,054 rows and 4 columns:
+#' \describe{
+#'   \item{location}{Location name}
+#'   \item{date}{Date of the delivery or receipt}
+#'   \item{deliveries}{Number of deliveries}
+#'   \item{receipts}{Number of receipts}
+#' }
+"enron_data"
