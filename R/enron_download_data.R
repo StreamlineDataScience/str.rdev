475c8f34f4232662be907a6d39f0881ee44fa6dc
Monica Gerber <mwgerber@gmail.com>
2023-03-09 18:52:30 -0800

    Create enron_download_data()

--- R/enron_download_data.R
+++ R/enron_download_data.R
@@ -1,4 +1,17 @@
+#' Download the Enron XLS file
+#'
+#' @param destfile Destination for XLS file.
+#'
+#' @return A XLS file saved at the specified file path.
 #' @export
-enron_download_file = function() {
-
+#'
+#' @examples
+#' \dontrun{
+#' enron_download_data("~/Desktop/enron_data.xls")
+#' }
+enron_download_data <- function(destfile) {
+  utils::download.file(
+    url = "https://github.com/StreamlineDataScience/enron-example/blob/main/andrea_ring_000_1_1.pst.0.xls?raw=true",
+    destfile = destfile
+  )
 }
