f97bfeeeb20c5f3463399f6a00d5a3b1a08ac21b
Monica Gerber <mwgerber@gmail.com>
2023-03-10 11:41:36 -0800

    Add package data

---
+++ R/enron_tidy_data.R
@@ -0,0 +1,14 @@
+#' Enron deliveries and receipts data
+#'
+#' Tidied data from and Enron XLS spreadsheet
+#'
+#' @format ## `enron_tidy_data`
+#' A data frame with 2,054 rows and 4 columns:
+#' \describe{
+#'   \item{location}{Location}
+#'   \item{date}{Date in YYYY-MM-DD format}
+#'   \item{deliveries}{deliveries}
+#'   \item{receipts}{receipts}
+#' }
+#' @source <https://github.com/StreamlineDataScience/enron-example/blob/main/andrea_ring_000_1_1.pst.0.xls>
+"enron_tidy_data"
